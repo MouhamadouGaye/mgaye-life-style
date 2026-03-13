7a8c2bd27efa454aec9a80c9a3e3793b64b449fc
VotreNom <kobeameth@gmail.com>
2026-03-14 00:46:47 +0100

    Fixe UUID import

--- src/main/java/com/mgaye/blog/controller/PostController.java
+++ src/main/java/com/mgaye/blog/controller/PostController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*")
