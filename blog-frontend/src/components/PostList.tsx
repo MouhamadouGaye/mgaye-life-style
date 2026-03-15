0d7c62e75ec0b977941bf8b00b31ee8e6bc216db
VotreNom <kobeameth@gmail.com>
2026-03-15 20:56:32 +0100

    the best developer

--- blog-frontend/src/components/PostList.tsx
+++ blog-frontend/src/components/PostList.tsx
@@ -22,27 +22,17 @@ export default function PostList() {
       <h1 className="title">My Blog</h1>
 
       <div className="posts-grid">
-        {/* {posts.map((post) => (
-          <div className="post-card" key={post.id}>
-            <img src={post.imageUrl} alt={post.title} />
+        {posts &&
+          posts.map((post: any) => (
+            <Link to={`/post/${post.id}`} className="post-card" key={post.id}>
+              <img src={post.imageUrl} />
 
-            <div className="post-content">
-              <h3>{post.title}</h3>
-              <p>{post.content}</p>
-            </div>
-          </div>
-        ))} */}
-
-        {posts.map((post: any) => (
-          <Link to={`/post/${post.id}`} className="post-card" key={post.id}>
-            <img src={post.imageUrl} />
-
-            <div className="post-content">
-              <h3>{post.title}</h3>
-              <p>{post.content.substring(0, 120)}...</p>
-            </div>
-          </Link>
-        ))}
+              <div className="post-content">
+                <h3>{post.title}</h3>
+                <p>{post.content.substring(0, 120)}...</p>
+              </div>
+            </Link>
+          ))}
       </div>
     </div>
   );
